e650b4b13d424e4e9277316e7036d0c685da1208
s1n7ax <srineshnisala@gmail.com>
2023-01-12 22:58:06 +0530

    feat(dev): add rtp to dev/init.lua file so no need of cmd args

--- dev/init.lua
+++ dev/init.lua
@@ -1,32 +1,34 @@
+local cwd = vim.fn.getcwd()
+vim.opt.runtimepath:prepend(cwd)
+
 --[[
 -- plugin name will be used to reload the loaded modules
 --]]
 local package_name = 'react'
 
 -- add the escape character to special characters
-local escape_pattern = function (text)
-    return text:gsub("([^%w])", "%%%1")
-end
+--  local escape_pattern = function (text)
+--  return text:gsub("([^%w])", "%%%1")
+--  end
 
 -- unload loaded modules by the matching text
-local unload_packages = function ()
-	local esc_package_name = escape_pattern(package_name)
-
+function Unload_packages()
 	for module_name, _ in pairs(package.loaded) do
-		if string.find(module_name, esc_package_name) then
+		if string.find(module_name, '^' .. package_name) then
 			package.loaded[module_name] = nil
 		end
 	end
 end
 
 -- executes the run method in the package
-local run_action = function ()
-	require(package_name).run()
+local run_action = function()
+	require('react').run()
+	--  vim.cmd('luafile test.lua')
 end
 
 -- unload and run the function from the package
 function Reload_and_run()
-	unload_packages()
+	Unload_packages()
 	run_action()
 end
 
@@ -34,3 +36,4 @@ local set_keymap = vim.api.nvim_set_keymap
 
 set_keymap('n', '<leader><leader>r', '<cmd>luafile dev/init.lua<cr>', {})
 set_keymap('n', '<leader><leader>w', '<cmd>lua Reload_and_run()<cr>', {})
+set_keymap('n', '<leader><leader>u', '<cmd>lua Unload_packages()<cr>', {})
